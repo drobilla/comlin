ccdd5b74c71462161652fc65cf8a8c0c6cbc3903
David Robillard <d@drobilla.net>
2026-04-10 15:21:26 -0400

    Use flags to reduce the configuration API

--- src/comlin.c
+++ src/comlin.c
@@ -121,24 +121,6 @@ refresh_line(ComlinState* l);
 
 /* Terminal Communication */
 
-void
-comlin_mask_mode_enable(ComlinState* const comlin)
-{
-    comlin->maskmode = true;
-}
-
-void
-comlin_mask_mode_disable(ComlinState* const comlin)
-{
-    comlin->maskmode = false;
-}
-
-void
-comlin_set_multi_line(ComlinState* const comlin, bool const ml)
-{
-    comlin->mlmode = ml;
-}
-
 static ssize_t
 read_full(int const fd, char* const buf, size_t const count)
 {
@@ -936,6 +918,14 @@ comlin_new_state(int const in_fd,
         l->history_max_len = max_history_len;
     }
     return l;
+}
+
+ComlinStatus
+comlin_set_mode(ComlinState* const state, ComlinModeFlags const flags)
+{
+    state->mlmode = flags & (ComlinModeFlags)COMLIN_MODE_MULTI_LINE;
+    state->maskmode = flags & (ComlinModeFlags)COMLIN_MODE_MASKED;
+    return COMLIN_SUCCESS;
 }
 
 ComlinStatus
