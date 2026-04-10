218fd140f2ce757d6c566c704c8bcd8166a7710c
David Robillard <d@drobilla.net>
2026-04-10 15:17:54 -0400

    Make everything possible const
    The less mutability, the less anyone needs to think about, better.

--- examples/example.c
+++ examples/example.c
@@ -15,7 +15,7 @@
 #include <string.h>
 
 static void
-completion(char const* buf, ComlinCompletions* lc)
+completion(char const* buf, ComlinCompletions* const lc)
 {
     if (buf[0] == 'h') {
         comlin_add_completion(lc, "hello");
@@ -74,8 +74,9 @@ printKeyCodesLoop(void)
 int
 main(int argc, char** argv)
 {
+    char* const prgname = argv[0];
+
     char const* line = NULL;
-    char* prgname = argv[0];
     int async = 0;
     int multiline = 0;
 
@@ -142,7 +143,7 @@ main(int argc, char** argv)
                 tv.tv_sec = 1; // 1 sec timeout
                 tv.tv_usec = 0;
 
-                int retval = select(1, &readfds, NULL, NULL, &tv);
+                int const retval = select(1, &readfds, NULL, NULL, &tv);
                 if (retval == -1) {
                     perror("select()");
                     return 1;
@@ -186,7 +187,7 @@ main(int argc, char** argv)
             comlin_history_save(state, "history.txt"); // Save history to disk
         } else if (!strncmp(line, "/historylen", 11)) {
             // The "/historylen" command will change the history len
-            int len = atoi(line + 11);
+            int const len = atoi(line + 11);
             comlin_history_set_max_len(state, (size_t)len);
         } else if (!strncmp(line, "/mask", 5)) {
             comlin_mask_mode_enable(state);
