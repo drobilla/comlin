38312b82dc9536b3ab220ad0adc6a1bc47fef50a
David Robillard <d@drobilla.net>
2026-04-10 15:16:50 -0400

    Make all type names start with an uppercase letter

--- examples/example.c
+++ examples/example.c
@@ -16,7 +16,7 @@
 #include <strings.h>
 
 static void
-completion(const char* buf, comlinCompletions* lc)
+completion(const char* buf, ComlinCompletions* lc)
 {
     if (buf[0] == 'h') {
         comlinAddCompletion(lc, "hello");
