dd433a17fcd118da3a6841ecbbcde3be95255512
David Robillard <d@drobilla.net>
2026-06-03 22:31:10 -0400

    Add header warnings test

---
+++ test/headers/test_headers.c
@@ -0,0 +1,11 @@
+// Copyright 2026 David Robillard <d@drobilla.net>
+// SPDX-License-Identifier: ISC
+
+#include <comlin/comlin.h> // IWYU pragma: keep
+
+int
+main(int argc, char** argv)
+{
+    (void)argv;
+    return argc != 1;
+}
